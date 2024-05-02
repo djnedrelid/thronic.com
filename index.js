971f20ade4947a60c10e441de3de2f7ca0796222
djnedrelid <thronic@gmail.com>
2024-05-02 11:32:41 +0200

    Oppdatering

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function header() {
 	document.write(
-		'<h1><a href="/">thronic.com</a></h1>'+
-		'<em>Keeping it simple.</em>'
+		'<a href="/"><img src="/gfx/thronic_header_2.png"></a><br>'+
+		'<em>By Dag J. V. Nedrelid. Just a norwegian plagued by deep curiosity.</em>'
 	);
 }
 
@@ -11,13 +11,16 @@ function footer() {
 		'<span style="font-weight:bold; cursor:help;" title="&copy; 2007-'+ d.getFullYear() +' Dag J. V. Nedrelid">ժʝ</span>&nbsp;&nbsp;'+
 		'<span onclick="mailme()" class="footer-dj-links" cursor:pointer;" title="E-mail">&#128231;</span> '+
 		'<a href="https://github.com/djnedrelid" class="footer-dj-links" target="_blank" title="GitHub">&#128008;</a> '+
-		'<a href="https://youtube.com/@thr0nic" class="footer-dj-links" target="_blank" title="YouTube">&#128250;</a> '
+		'<a href="https://youtube.com/@thr0nic" class="footer-dj-links" target="_blank" title="YouTube">&#128250;</a> '+
+		'<br><br>'+
+		'&copy;2007-'+ d.getFullYear() +'.<br>'+
+		'DAG J NEDRELID SOFTWARE ENK.<br>Org.nr. 930295590 - Open for freelancing.'
 	);
 }
 
 function backlink() {
 	document.write(
-		'<a href="/" class="back-link" title="Go back to the front page.">&larr; &#127968;</a>'
+		'<a href="/" class="back-link" title="Go back to the front page.">&#127968;</a>'
 	);
 }
 
@@ -38,7 +41,7 @@ function mailme() {
 	for (let a=0; a<m.length; a++)
 		e += m[c[a]];
 	
-	location.href = e;
+	window.open(e, '_blank');
 }
 
 //
@@ -46,27 +49,33 @@ function mailme() {
 //
 function NotatMeny(unntak) {
 	var Notater = [
-		'<span class="menuheader">ELECTRIC</span>',
-		'<a href="/Notes/electric_silicone_and_circuit_boards.html">Silicone and Circuit Boards</a>',
+		'<span class="menuheader">Projects</span>',
+		'<a href="/Notes/featured_code_projects.html">Featured</a>',
+		'<a href="/Notes/tools.html">Online Tools</a>',
 		'<br>',
-		'<span class="menuheader">GNU/LINUX</span>',
+		'<span class="menuheader">GNU/Linux Notes</span>',
 		'<a href="/Notes/linux_ddrescue.html">ddrescue (N)</a>',
 		'<a href="/Notes/linux_iptables_firewall_and_gateway.html">Iptables Firewall and Gateway</a>',
+		'<a href="/Notes/linux_kvm_hypervisor_setup.html">KVM Hypervisor Setup</a>',
 		'<br>',
-		'<span class="menuheader">HTML &amp; Client side</span>',
+		'<span class="menuheader">HTML &amp; JS Notes</span>',
 		'<a href="/Notes/html_skjelett.html">HTML Skjelett (N)</a>',
 		'<a href="/Notes/ajax_skjelett.html">AJAX Skjelett (N)</a>',
 		'<br>',
-		'<span class="menuheader">MISC</span>',
-		'<a href="/Notes/misc_plex_media_server_migration.html">Plex Media Server Migration</a>',
-		'<a href="/Notes/misc_the_hexadecimal_system.html">The Hexadecimal System</a>',
-		'<br>',
-		'<span class="menuheader">WINDOWS</span>',
+		'<span class="menuheader">Microsoft Windows Notes</span>',
 		'<a href="/Notes/windows_common_commands.html">Common Run Commands</a>',
+		'<a href="/Notes/windows_hyperv_replication.html">Hyper-V Replication</a>',
 		'<a href="/Notes/windows_inaccessible_boot.html">INACCESSIBLE_BOOT_DEVICE</a>',
 		'<a href="/Notes/windows_batch_norske_tegn.html">Norske tegn i batch (N)</a>',
 		'<a href="/Notes/windows_reparere_oppstartspartisjoner.html">Reparere BCD (N)</a>',
-		'<a href="/Notes/windows_securing_rdp.html">Securing RDP</a>'
+		'<a href="/Notes/windows_securing_rdp.html">Securing RDP</a>',
+		'<br>',
+		'<span class="menuheader">Various Notes</span>',
+		'<a href="/Notes/misc_openvpn_site_to_site.html">OpenVPN Site-to-Site</a>',
+		'<a href="/Notes/misc_plex_media_server_migration.html">Plex Media Server Migration</a>',
+		'<a href="/Notes/electric_silicone_and_circuit_boards.html">Silicone and Circuit Boards</a>',
+		'<a href="/Notes/misc_ssh_tunneling.html">SSH Tunneling</a>',
+		'<a href="/Notes/misc_the_hexadecimal_system.html">The Hexadecimal System</a>',
 	];
 	
 	for (let a=0; a<Notater.length; a++) {
